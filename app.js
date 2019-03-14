7b058a3b7e649da73bfed6e69853ad91b7e18071
rstutalo <richardtutalo@gmail.com>
2019-03-13 21:05:23 -0600

    user input function working as expected.

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 var mysql = require("mysql");
-var inquirer = require("inquirer");
 var table = require("console.table");
+var inquirer = require("inquirer");
+
 
 var connection = mysql.createConnection({
     host: "localhost",
@@ -13,27 +14,41 @@ var connection = mysql.createConnection({
 connection.connect(function(err, response){
     if (err) throw err; // throw error if something wrong with connection
     // display thread ID to confirm connection
-    console.log("Connected with id: " + connection.threadID);
-    // display database with function
-    displayDb();
-    // run userPropt function here!
+    console.log("\nConnected with id: " + connection.threadID + "\n");
+    startApp()
 });
 
 // function to display database when app runs
-function displayDb() {
+function startApp() {
     connection.query("SELECT item_id, product_name, price FROM products", function(err, response) {
         // throw error if something wrong with query
         if (err) throw err;
-        // console log the response(full database at this point)
-        // var output = table(response.data);
         console.table(response);
+        userInput();
         connection.end();
     })
 }
 
-function userPrompts() {
-
-}
-
-
-
+function userInput() {
+    // prompt users to ask for the id of the product they want to buy
+    inquirer
+        .prompt([
+            {
+                name: "itemInquiry",
+                type: "input",
+                message: "Please enter the id of the item you would like to shop for:"
+                
+            },
+            //prompt user to ask how many units of that product they would like to buy.
+            {
+                name: "quantityInquiry",
+                type: "input",
+                message: "Please enter the number of items you would like to purchase:"
+            }
+             //THEN
+            //take user input (id and quant) & check it user quant is >= id iventory
+        ]).then(function(answer){
+            console.log(answer.itemInquiry);
+            console.log(answer.quantityInquiry);
+        })
+    }
