250b0a800844399bc69aecf0636bead3b0236fb7
rstutalo <richardtutalo@gmail.com>
2019-03-17 17:40:42 -0600

    store app working but there are some error message affecting the ending output.

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
-var mysql = require("mysql");
-var table = require("console.table");
-var inquirer = require("inquirer");
+const mysql = require("mysql");
+const table = require("console.table");
+const inquirer = require("inquirer");
+var dot = require("dotenv");
+
 
 
 var connection = mysql.createConnection({
@@ -11,21 +13,23 @@ var connection = mysql.createConnection({
     database: "bamazon_db"
 });
 
-connection.connect(function(err, response){
-    if (err) throw err; // throw error if something wrong with connection
-    // display thread ID to confirm connection
+connection.connect(function(err){
+    if (err) {
+        console.error("Error connecting: " + err.stack);
+        return;
+    }
     console.log("\nConnected with id: " + connection.threadID + "\n");
-    startApp()
+    // startApp()
 });
 
 // function to display database when app runs
 function startApp() {
     connection.query("SELECT item_id, product_name, price FROM products", function(err, response) {
-        // throw error if something wrong with query
+        // catch error/exception if one is thrown if something wrong with query
         if (err) throw err;
         console.table(response);
         userInput();
-        connection.end();
+        // connection.end();
     })
 }
 
@@ -34,21 +38,79 @@ function userInput() {
     inquirer
         .prompt([
             {
-                name: "itemInquiry",
                 type: "input",
-                message: "Please enter the id of the item you would like to shop for:"
+                name: "item_id",
+                message: "Please enter the id of the item you would like to shop for:",
+                filter: Number
                 
             },
             //prompt user to ask how many units of that product they would like to buy.
             {
-                name: "quantityInquiry",
                 type: "input",
-                message: "Please enter the number of items you would like to purchase:"
+                name: "quantity",
+                message: "Please enter the number of items you would like to purchase:",
+                validate: function(value) {
+                    if (isNaN(value) === false) {
+                        return true;
+                    }
+                    return false;
+                },
+                filter: Number
             }
-             //THEN
-            //take user input (id and quant) & check it user quant is >= id iventory
-        ]).then(function(answer){
-            console.log(answer.itemInquiry);
-            console.log(answer.quantityInquiry);
-        })
+        ]).then(function(input){
+            var userRequest = input.item_id;
+            var quantity = input.quantity;
+
+            var newQuery = "SELECT * FROM products WHERE ?";
+
+            connection.query(newQuery, {item_id: userRequest}, function(err, data){
+                  if (err) throw err;
+                  connection.on('err', function(err) {
+                      console.log("[mysql err]", err);
+                  });
+                if (data.length === 0) {
+                    console.log("\n" + "********************");
+                    console.log("Error: Invalid data type. Please enter a valid ID");
+                    console.log("********************");
+                    console.log("Restarting Application..." + "\n");
+                    startApp();
+                } else {
+                    // compare user quant request to count of item in stock
+                    var merchData = data[0];
+                    if (quantity <= merchData.stock_quantity) {
+                        console.log("\nWe have that amount in stock! Your order is being placed! ");
+                        console.log("Thank you for your business!\n");
+                            console.log("********************");
+                            console.log("The total charge for this order is: " + "$" + merchData.price * quantity);
+                            console.log("********************");
+                        //create new SQL query for updating the database
+                        var queryUpdate = "UPDATE products SET stock_quantity = " + (merchData.stock_quantity - quantity) + "WHERE item_id = " + userRequest
+
+                        connection.query(queryUpdate, function(err, data){
+                            if (err) throw err; 
+                            //break connection to database once order is placed
+                            connection.end();
+                        })
+                    } else {
+                        console.log("\nApologies. We currently do not have that amount of product in stock.");
+                        console.log("\n********************");
+                        console.log("\nPlease check back later or modify the quantity of your order.\n");
+                        //break connection if it gets to this point
+                        connection.end();
+                    }
+                }
+            });
+        });
     }
+
+    //run application
+    startApp();
+        
+        
+        
+        
+        
+        
+        
+        
+        
---
+++ manager.js
@@ -0,0 +1,5 @@
+var fs = require("fs");
+var inquirer = require("inquirer");
+var mysql = require("mysql");
+var dot = require("dotenv");
+
